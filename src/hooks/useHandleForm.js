286496b80a4d10a838627ef068139d42a79cbd8a
Salyens <salyens@yandex.ru>
2023-10-17 13:35:32 +0400

    update

--- src/hooks/useHandleForm.js
+++ src/hooks/useHandleForm.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
-
 const useHandleForm = (initialState, serviceFunction, navigateFunction) => {
   const [input, setInput] = useState(initialState);
   const [errors, setErrors] = useState([]);
-  
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setInput((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async () => {
-
     try {
       await serviceFunction(input);
       setInput(initialState);
       navigateFunction();
     } catch (error) {
-      setErrors(error.response.data.message);
+      Array.isArray(error.response.data.message)
+        ? setErrors(error.response.data.message)
+        : setErrors([error.response.data.message]);
     }
   };
 
